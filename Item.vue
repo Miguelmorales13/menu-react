dae6b2e6ff78f962a7003468bd39b8d059b8a845
Miguel Angel Morales <miguel.moralesr@hotmail.com>
2021-03-05 22:34:56 -0600

    first commit

--- Item.vue
+++ Item.vue
@@ -1,21 +1,22 @@
 <template>
-	<div id="Item">
+  <div id="Item">
 		<span v-for="(head,i) of headers" :key="i">
-			<b>{{$t(head.label)}}:&nbsp;</b>
-			<span>{{item[head.field]}}</span>
-			<br />
+			<b>{{ $t(head.label) }}:&nbsp;</b>
+			<span>{{ item[head.field] }}</span>
+			<br/>
 		</span>
-	</div>
+  </div>
 </template>
 
 <script lang="ts">
-	import { Component, Prop, Vue } from "vue-property-decorator";
+import {Component, Prop, Vue} from "vue-property-decorator";
 
-	@Component
-	export default class Item extends Vue {
-		@Prop({ type: Object, required: true }) item!: any;
-		@Prop({ type: Array, required: true }) headers!: any;
-	}
+@Component
+export default class Item extends Vue {
+  @Prop({type: Object, required: true}) item!: any;
+  @Prop({type: Array, required: true}) headers!: any;
+}
+	
 </script>
 
 <style lang="scss" scope></style>
